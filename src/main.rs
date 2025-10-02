1b21dd92d3949763377ff65442affb6573988df6
Paul Borrego (Personal) <borregopaulj@gmail.com>
2025-10-02 17:41:46 -0500

    Feat: implemented basic server, need to update html for better cmds

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,79 @@
 use axum::{
-    Router,
-    extract::Query,
-    response::{Html,IntoResponse},
+    Json, Router,
+    response::{Html, IntoResponse},
     routing::get,
 };
 
-use axum_extra::response::Css;
+use axum_extra::response::{Css, JavaScript};
 
 use hypertext::{GlobalAttributes, Renderable, html_elements, maud};
-
 use serde::Deserialize;
+
+mod back;
+
+use crate::back::server::{Action, Server, manipulate_server};
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
-    
-    let app = Router::new()
-        .merge(route_schedule());
+    let app = Router::new().merge(route_base());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on http://{}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
-fn route_schedule() -> Router {
+fn route_base() -> Router {
     Router::new()
         .route("/", get(handler_index))
         .route("/schedule", get(handler_schedule))
-        .route("/server", get(handler_server))
+        .route("/server", get(handler_server).post(handler_server_put))
         .route("/style/server.css", get(style_server))
         .route("/style/schedule.css", get(style_schedule))
+        .route("/scripts/server.js", get(script_server))
 }
 
-#[derive(Debug, Deserialize)]
-struct ScheduleRequest {
-    task: Option<String>,
-}
-
-// /scheuler?task=example
-async fn handler_schedule(Query(params): Query<ScheduleRequest>) -> impl IntoResponse {
-    println!("--> schedule_handler - {:?}", params);
+async fn handler_schedule() -> impl IntoResponse {
+    println!("--> schedule_handler -");
 
     let ppls = vec![
-        People { name: String::from("Nora"), status: false },
-        People { name: String::from("Paul"), status: false },
-        People { name: String::from("Logan"), status: false },
-        People { name: String::from("Cisco"), status: false },
-        People { name: String::from("Seve"), status: false },
-        People { name: String::from("Brown"), status: false },
-        People { name: String::from("Simmons"), status: false },
-        People { name: String::from("Garret"), status: false },
-        People { name: String::from("Dave"), status: false },
+        People {
+            name: String::from("Nora"),
+            status: false,
+        },
+        People {
+            name: String::from("Paul"),
+            status: false,
+        },
+        People {
+            name: String::from("Logan"),
+            status: false,
+        },
+        People {
+            name: String::from("Cisco"),
+            status: false,
+        },
+        People {
+            name: String::from("Seve"),
+            status: false,
+        },
+        People {
+            name: String::from("Brown"),
+            status: false,
+        },
+        People {
+            name: String::from("Simmons"),
+            status: false,
+        },
+        People {
+            name: String::from("Garret"),
+            status: false,
+        },
+        People {
+            name: String::from("Dave"),
+            status: false,
+        },
     ];
-    let task = params.task.as_deref().unwrap_or("No task provided");
     let format = maud! {
         link rel="stylesheet" type="text/css" href=("style/schedule.css"); // class is . , id is #
         h1 #remag {
@@ -67,25 +88,25 @@ async fn handler_schedule(Query(params): Query<ScheduleRequest>) -> impl IntoRes
 
             }
         }
-    }.render();
+    }
+    .render();
     Html(format.into_inner())
 }
-
 
 async fn handler_index() -> impl IntoResponse {
     println!("--> - handler_index -");
 
     let format = maud! {
-        link rel="stylesheet" href=("css/schedule.css");
+        link rel="stylesheet" href=("sytle/schedule.css");
         div {
-            h1 { "Index" }
+            h1 { "Paul's Site" }
             ul {
-                li { "one" }
-                li { "two" }
-                li { "three" }
+                li { a href="/server" {"Go To Server"} }
+                li { a href="/schedule" {"Go To Schedule"} }
             }
         }
-    }.render();
+    }
+    .render();
     Html(format.into_inner())
 }
 
@@ -93,27 +114,60 @@ async fn handler_server() -> impl IntoResponse {
     println!("--> - handler_server -");
 
     let format = maud! {
-        link rel="stylesheet" href=("/style.css");
+        link rel="stylesheet" href=("style/server.css");
+        script src="scripts/server.js" {}
         div {
             h1 { "Server" }
             ul {
-                li { "Bedock" }
-                li { "Java" }
+                button id="bedrock" {" Bedrock "}
+                button id="java" {" Java "}
             }
         }
-        img src="hehe.jpg";
-    }.render();
+    }
+    .render();
     Html(format.into_inner())
 }
 
+#[derive(Debug, Deserialize)]
+struct ServerRequest {
+    server: String,
+    action: String,
+}
+
+async fn handler_server_put(payload: Json<ServerRequest>) -> impl IntoResponse {
+    let server = &payload.server;
+    let action = &payload.action;
+
+    let s = match server.as_str() {
+        "bedrock" => Server::Bedrock,
+        "java" => Server::Java,
+        _ => Server::Other,
+    };
+
+    let a = match action.as_str() {
+        "start" => Action::Start,
+        "restart" => Action::Restart,
+        "close" => Action::Close,
+        _ => Action::Other,
+    };
+
+    let res = manipulate_server(s, a);
+    println!("server: {:?}, Action: {:?} | {:?}", server, action, res);
+}
+
 async fn style_server() -> Css<&'static str> {
-    let server_css: &str = include_str!("../css/server.css");
+    let server_css: &str = include_str!("../public/css/server.css");
     Css(server_css)
 }
 
 async fn style_schedule() -> Css<&'static str> {
-    let server_css: &str = include_str!("../css/schedule.css");
+    let server_css: &str = include_str!("../public/css/schedule.css");
     Css(server_css)
+}
+
+async fn script_server() -> JavaScript<&'static str> {
+    let server_script: &str = include_str!("../public/scripts/server.js");
+    JavaScript(server_script)
 }
 
 struct People {
--- watch_test.exe
+++
@@ -1,1 +0,0 @@
-cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"
